3b4aacf0761d34a8773bd3ff2ab8a383387e930b
samkimuel <samkim2626@gmail.com>
2021-11-29 01:40:30 +0900

    chore: 기본 test 설정

---
+++ src/test/java/com/matchus/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.matchus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
