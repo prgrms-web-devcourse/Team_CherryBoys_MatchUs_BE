b3ed97530e3adc801f9010c5804eb6ed171f8e36
sunH0 <sunho6824@daum.net>
2021-12-08 15:58:25 +0900

    [MAT-96] feature: 회원가입 & 로그인 API 구현

--- src/main/java/com/matchus/domains/user/dto/LoginResponse.java
+++ src/main/java/com/matchus/domains/user/dto/LoginResponse.java
@@ -1,19 +1,24 @@
 package com.matchus.domains.user.dto;
 
 import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class LoginResponse {
 
-	private final TokenResponse tokenResponse;
+	private final String token;
+	private final String group;
 	private final List<UserGrade> userGrades;
 
+	@Builder
 	public LoginResponse(
-		TokenResponse tokenResponse,
+		String token,
+		String group,
 		List<UserGrade> userGrades
 	) {
-		this.tokenResponse = tokenResponse;
+		this.token = token;
+		this.group = group;
 		this.userGrades = userGrades;
 	}
 
