d3c721d7c3412ffa03622fad8136708d6f4711d4
sunH0 <sunho6824@daum.net>
2021-12-10 05:58:37 +0900

    [MAT-125] fix: 빌드 오류 제거

--- src/test/java/com/matchus/domains/user/service/UserServiceTest.java
+++ src/test/java/com/matchus/domains/user/service/UserServiceTest.java
@@ -4,13 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import com.matchus.domains.common.dto.SuccessResponse;
-import com.matchus.domains.sports.domain.Sports;
 import com.matchus.domains.sports.service.SportsService;
 import com.matchus.domains.team.service.TeamUserService;
 import com.matchus.domains.user.converter.UserConverter;
-import com.matchus.domains.user.domain.Group;
-import com.matchus.domains.user.domain.User;
-import com.matchus.domains.user.dto.SignUpRequest;
 import com.matchus.domains.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,41 +38,6 @@ class UserServiceTest {
 	private SportsService sportsService;
 	@Spy
 	private BCryptPasswordEncoder passwordEncoder;
-
-
-	@Test
-	@DisplayName("회원가입 성공 테스트")
-	void SignUpTest() {
-		//given
-		final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-		final SignUpRequest signUpRequest = SignUpRequest
-			.builder()
-			.name("오재원")
-			.email("abc@abc.com")
-			.nickname("채리")
-			.password("password")
-			.gender("MAN")
-			.ageGroup("20대")
-			.sports("축구")
-			.bio("안녕세요")
-			.build();
-
-		given(sportsService.getSports(signUpRequest.getSports()))
-			.willReturn(any(Sports.class));
-		given(groupService.findByName(any(String.class)))
-			.willReturn(any(Group.class));
-
-		final String encryptedPw = encoder.encode(signUpRequest.getPassword());
-
-		//when
-		userService.signUp(signUpRequest);
-
-		//then
-		verify(userRepository, times(1)).save(any(User.class));
-		verify(passwordEncoder, times(1)).encode(any(String.class));
-
-	}
 
 	@Test
 	@DisplayName("이메일 중복 여부 테스트")
