67793ad384d1088f10c115e2ae376bacea3ddee2
sunH0 <sunho6824@daum.net>
2021-12-10 05:30:36 +0900

    [MAT-125] feature: 이메일, 닉네임 중복확인 API 구현

--- src/main/java/com/matchus/domains/user/repository/UserRepository.java
+++ src/main/java/com/matchus/domains/user/repository/UserRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	Optional<User> findByEmail(String email);
+	Optional<User> findByEmailAndIsDisaffiliatedFalse(String email);
+
+	boolean existsByEmail(String email);
+
+	boolean existsByNickname(String nickname);
 
 }
