1e87aae7a4531df6ccdd79ef289eb0e0a1cd7dad
samkimuel <samkim2626@gmail.com>
2021-12-11 21:34:37 +0900

    [MAT-154] feat:  City, Region, Ground repository 구현

---
+++ src/main/java/com/matchus/domains/location/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.matchus.domains.location.repository;
+
+import com.matchus.domains.location.domain.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+
+}
