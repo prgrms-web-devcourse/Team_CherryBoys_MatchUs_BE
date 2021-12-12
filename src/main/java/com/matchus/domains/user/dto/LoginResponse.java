2513d620ed5061a06c4a666107acda9227f52c4e
sunH0 <sunho6824@daum.net>
2021-12-12 18:48:03 +0900

    [MAT-160] fix:  로그인 response 수정

--- src/main/java/com/matchus/domains/user/dto/LoginResponse.java
+++ src/main/java/com/matchus/domains/user/dto/LoginResponse.java
@@ -3,7 +3,6 @@ package com.matchus.domains.user.dto;
 import com.matchus.domains.common.AgeGroup;
 import com.matchus.domains.team.domain.Grade;
 import com.matchus.domains.user.domain.Gender;
-import java.math.BigDecimal;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,41 +26,37 @@ public class LoginResponse {
 	public static class UserResponse {
 
 		private final Long id;
-		private final String sport;
-		private final String email;
 		private final String name;
+		private final String nickname;
 		private final String bio;
+		private final String sports;
 		private final Gender gender;
 		private final AgeGroup ageGroup;
 		private final String roleGroup;
-		private final BigDecimal mannerTemperature;
 		private final List<UserGradeResponse> userGradeResponse;
 
 		@Builder
 		public UserResponse(
 			Long id,
-			String sport,
-			String email,
 			String name,
+			String nickname,
 			String bio,
+			String sports,
 			Gender gender,
 			AgeGroup ageGroup,
 			String roleGroup,
-			BigDecimal mannerTemperature,
 			List<UserGradeResponse> userGradeResponse
 		) {
 			this.id = id;
-			this.sport = sport;
-			this.email = email;
 			this.name = name;
+			this.nickname = nickname;
 			this.bio = bio;
+			this.sports = sports;
 			this.gender = gender;
 			this.ageGroup = ageGroup;
 			this.roleGroup = roleGroup;
-			this.mannerTemperature = mannerTemperature;
 			this.userGradeResponse = userGradeResponse;
 		}
-
 	}
 
 	@Getter
