d775c97fcc33b0b236cc40484cf2e9102e235896
samkimuel <samkim2626@gmail.com>
2021-12-13 16:05:35 +0900

    [MAT-135] style:  Swagger 문서화 추가

--- src/main/java/com/matchus/domains/hire/controller/HireApplicationController.java
+++ src/main/java/com/matchus/domains/hire/controller/HireApplicationController.java
@@ -5,6 +5,7 @@ import com.matchus.domains.hire.dto.response.HireApplyResponse;
 import com.matchus.domains.hire.service.HireApplicationService;
 import com.matchus.global.jwt.JwtAuthentication;
 import com.matchus.global.response.ApiResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,6 +21,10 @@ public class HireApplicationController {
 
 	private final HireApplicationService hireApplicationService;
 
+	@ApiOperation(
+		value = "용병 신청",
+		notes = "용병 구인 게시글에서 용병 신청 버튼을 통해 용병 신청을 할 수 있습니다."
+	)
 	@PostMapping
 	public ResponseEntity<ApiResponse<HireApplyResponse>> applyHire(
 		@RequestBody HireApplyRequest request,
