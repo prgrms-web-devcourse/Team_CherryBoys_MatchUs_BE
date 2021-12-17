ca26694fba43f35c79f0956fac792c69317b9808
sunH0 <sunho6824@daum.net>
2021-12-17 16:08:05 +0900

    [MAT-224] fix:  매치 리스트 조회 Response의 AgeGroup을 Enum->String 수정

---
+++ src/main/java/com/matchus/domains/match/dto/response/MatchListByFilterResult.java
@@ -0,0 +1,40 @@
+package com.matchus.domains.match.dto.response;
+
+import com.matchus.domains.common.AgeGroup;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MatchListByFilterResult {
+
+	private final Long matchId;
+
+	private final String city;
+
+	private final String region;
+
+	private final String ground;
+
+	private final LocalDate date;
+
+	private final LocalTime startTime;
+
+	private final LocalTime endTime;
+
+	private final AgeGroup ageGroup;
+
+	private final String sports;
+
+	private final Long teamId;
+
+	private final String teamLogo;
+
+	private final String teamName;
+
+	private final BigDecimal mannerTemperature;
+
+}
