2bff16dfe656f320069424fbeb9f4f19d5355761
sunH0 <sunho6824@daum.net>
2021-12-19 15:50:34 +0900

    [MAT-248] refactor:  지원한 용병 게시글 리스트 응답 데이터 수정

--- src/main/java/com/matchus/domains/hire/domain/HireApplyTeam.java
+++ src/main/java/com/matchus/domains/hire/domain/HireApplyTeam.java
@@ -5,11 +5,18 @@ import lombok.Getter;
 @Getter
 public class HireApplyTeam {
 
+	private final Long applicationId;
 	private final Long hirePostId;
 	private final Long teamId;
 	private final String teamName;
 
-	public HireApplyTeam(Long hirePostId, Long teamId, String teamName) {
+	public HireApplyTeam(
+		Long applicationId,
+		Long hirePostId,
+		Long teamId,
+		String teamName
+	) {
+		this.applicationId = applicationId;
 		this.hirePostId = hirePostId;
 		this.teamId = teamId;
 		this.teamName = teamName;
