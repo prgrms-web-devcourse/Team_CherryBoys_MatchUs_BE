cc0673adfb9d3aae928c2bce790ea694cf0b7d71
samkimuel <samkim2626@gmail.com>
2022-01-12 15:50:41 +0900

    [MAT-290] refactor:  Service Layer 추가를 통해 리 팀 탈퇴 기능 구현 리팩토링

---
+++ src/main/java/com/matchus/domains/team/service/TeamUserJoining.java
@@ -0,0 +1,25 @@
+package com.matchus.domains.team.service;
+
+import com.matchus.domains.team.domain.TeamUser;
+import com.matchus.domains.team.dto.response.TeamIdResponse;
+import com.matchus.domains.user.domain.User;
+import com.matchus.domains.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TeamUserJoining {
+
+	private final TeamUserService teamUserService;
+	private final UserService userService;
+
+	public TeamIdResponse leaveTeam(Long teamId, String userEmail) {
+		User user = userService.findActiveUser(userEmail);
+		TeamUser teamUser = teamUserService.getTeamUser(teamId, user.getId());
+		teamUserService.delete(teamUser);
+
+		return new TeamIdResponse(teamId);
+	}
+
+}
